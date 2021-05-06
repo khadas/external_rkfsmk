7f1ad82a970db1bc956afa4140f0927224be815d
Jinkun Hong <jinkun.hong@rock-chips.com>
2021-05-06 14:29:33 +0800

    rkfsmk:add code
    Signed-off-by: Jinkun Hong <jinkun.hong@rock-chips.com>
    Change-Id: I4a58deb2a60d88dc0833e02506ffb59b91307bd4

---
+++ rkfsmk.h
@@ -0,0 +1,11 @@
+#ifndef __RKFSMK_H
+#define __RKFSMK_H
+
+int rkfsmk_start(void *handle);
+int rkfsmk_create(void **handle, char *device_name, char *volume_name, unsigned int align_size);
+void rkfsmk_add_dir(void *handle, char *path, int hidden);
+int rkfsmk_add_file(void *handle, char *path, char *filename, int hidden, int size);
+void rkfsmk_destroy(void *handle);
+unsigned long long rkfsmk_disk_size_get(void *handle);
+
+#endif
